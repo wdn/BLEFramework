4ee9fed8456a0d3cbf84639ca2eb32e2722be90c
W. Dana Nuon <wdnuon@gmail.com>
2015-07-27 17:17:20 -0400

    Fixes for Xcode 7 and iOS 8.

--- Examples/BLEController/BLEController/RBLDetailViewController.m
+++ Examples/BLEController/BLEController/RBLDetailViewController.m
@@ -49,9 +49,9 @@
             NSNumber *n = [ble.peripheralsRssi objectAtIndex:i];
             NSString *name = [[ble.peripherals objectAtIndex:i] name];
             
-            if (p.UUID != NULL)
+            if (p.identifier)
             {
-                [self.BLEDevices insertObject:[self getUUIDString:p.UUID] atIndex:i];
+                [self.BLEDevices insertObject:p.identifier.UUIDString atIndex:i];
                 [self.BLEDevicesRssi insertObject:n atIndex:i];
                 
                 if (name != nil)
@@ -66,7 +66,7 @@
             else
             {
                 [self.BLEDevices insertObject:@"NULL" atIndex:i];
-                [self.BLEDevicesRssi insertObject:0 atIndex:i];
+                [self.BLEDevicesRssi insertObject:@0 atIndex:i];
                 [self.BLEDevicesName insertObject:@"RedBear Device" atIndex:i];
             }
         }
@@ -139,12 +139,6 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [ble connectPeripheral:[ble.peripherals objectAtIndex:indexPath.row]];
-}
-
--(NSString *)getUUIDString:(CFUUIDRef)ref
-{
-    NSString *str = [NSString stringWithFormat:@"%@", ref];
-    return [[NSString stringWithFormat:@"%@", str] substringWithRange:NSMakeRange(str.length - 36, 36)];
 }
 
 @end
